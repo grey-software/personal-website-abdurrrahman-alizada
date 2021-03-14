4f49ea710b182d7699a655c3d7d12b8f1fc10b99
abdurrrahman-alizada <79741876+abdurrrahman-alizada@users.noreply.github.com>
2021-03-14 17:42:41 +0500

    add email button to contact page

--- pages/Contact.vue
+++ pages/Contact.vue
@@ -1,15 +1,15 @@
 <template>
-    <div>
-        <section class="h-screen flex text-gray-600 body-font mx-20 relative">
+    <div class="">
+        <section class="flex text-gray-600 body-font mx-20">
    <img class="h-30 w-1/3" src="@/assets/svgs/contact.svg"/>
   <div class="container  px-5 py-24 mx-auto">
     <div class="flex items-center mx-auto flex-col text-center w-full mb-12">
       <h1 class="sm:text-3xl heading text-2xl font-bold title-font mb-4 text-gray-900">Contact Me</h1>
-      <p class="lg:w-2/3 mx-auto leading-relaxed text-black">You can contact me via InstaGram or E-mail. Feel free to check out my social media presence below to get to know me better.</p>
+      <p class="lg:w-2/3 mx-auto leading-relaxed text-black">You can contact me via InstaGram or E-mail (abdurrahmanalizada17@gmail.com). Feel free to check out my social media presence below to get to know me better.</p>
     </div>
 
   <div class="xl:mx-20">
-    <ul class="flex flex-wrap p-5 ">
+    <ul class="flex flex-wrap sm:-p-5 ">
       <li class="m-10 hover:bg-purple-900 rounded-3xl">
         <a href="https://github.com/Abdurrahman-Alizada"><img class="h-10 w-10 hover:text-purple-700" src="@/assets/svgs/g.svg"/></a>
       </li>
@@ -22,9 +22,11 @@
             <li class="m-10 hover:bg-blue-700 rounded-2xl">
         <a href="https://web.facebook.com/abdurrahman.suddais.1/"><img class="h-10 w-10" src="@/assets/svgs/f.svg"/></a>
       </li>
-            <!-- <li class="m-10 hover:bg-purple-800 rounded-xl">
-        <a href="https://www.instagram.com/ab.durrahman9254/"><img class="h-10 w-10" src="@/assets/svgs/i.svg"/></a>
-      </li> -->
+            <li class="m-10 hover:bg-yellow-500 rounded-full">
+        <a href="mailto:abdurrahmanalizada17@gmail.com"><img class="h-10 w-10" src="@/assets/svgs/gmail.svg"/></a>
+      </li>
+
+
     </ul>
   </div>
   </div>
